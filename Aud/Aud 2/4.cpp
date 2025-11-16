db3f01b587babdf0627881376d4adf162088ac8d
Metrox <metrox363@gmail.com>
2025-11-16 18:52:29 +0100

    Auditory exercise 2
    Implemented a program to calculate the arithmetic mean of the numbers 3, 5, and 12.

---
+++ Aud/Aud 2/4.cpp
@@ -0,0 +1,10 @@
+// Задача 4
+// Да се напише програма за пресметување на аритметичката средина на броевите 3, 5 и 12.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a=3, b=5, c=12;
+    float as = (a+b+c)/3.0;
+    cout<<as<<endl;
+}
