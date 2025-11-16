d9d436f3f3e62d4bcc23327a9d51b5f13a701949
Metrox <metrox363@gmail.com>
2025-11-16 13:59:56 +0100

    Laboratory Exercise 1

--- Labs/Labs 1/2.cpp
+++ Labs/Labs 1/2.cpp
@@ -1,6 +1,3 @@
-//
-// Created by metro on 11/8/2025.
-//
 /*
 Да се напише програма која за петцифрен број внесен од стандарден влез
 ќе ја пресмета сумата на цифритена парните и непарните позиции соодветно.
@@ -11,7 +8,6 @@
 Input: 54321
 Output: Sumata na cifrite na parnite pozicii e 9
         Sumata na cifrite na neparnite pozicii e 6
-
 */
 #include <iostream>
 using namespace std;
@@ -27,11 +23,12 @@ int main() {
                 if (i%2==0) {
                 ParniPozicii += cifra;
                 }else {
-                        NeParniPozicii += cifra;
+                NeParniPozicii += cifra;
                 }
                 broj=broj/10;
         }
         cout<<"Sumata na cifrite na parnite pozicii e "<< ParniPozicii<< endl;
         cout<<"Sumata na cifrite na neparnite pozicii e "<<NeParniPozicii<<endl;
 }
+
 
