08b0c70dc33339044a7b58620e572b04c35d3492
Marko <zdravkovskimarko6@gmail.com>
2025-12-14 11:37:00 +0100

    5.cpp

--- Aud/Aud 8/5.cpp
+++ Aud/Aud 8/5.cpp
@@ -1,3 +1,15 @@
-//
-// Created by metro on 11/16/2025.
-//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[100];
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    int posleden = a[n-1];
+    for (int i = n-1; i > 0; i--) {
+        a[i] = a[i-1];
+    }
+}
