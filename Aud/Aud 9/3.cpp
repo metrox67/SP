c1aad7c8871835a1ad94b272cdb999058517e83e
Marko <zdravkovskimarko6@gmail.com>
2025-12-14 11:43:16 +0100

    3.cpp

--- Aud/Aud 9/3.cpp
+++ Aud/Aud 9/3.cpp
@@ -1,3 +1,23 @@
-//
-// Created by metro on 11/16/2025.
-//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[100][100];
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> a[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            if (a[i][j] != a[j][i]) {
+                cout<<"Ne e simetricna"<<endl;
+                return 0;
+            }
+        }
+    }
+    cout <<"Simetricna e"<< endl;
+    return 0;
+}
