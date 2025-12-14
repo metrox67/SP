d9b7d875a2714509f93c365e23a6c53916d20d08
Marko <zdravkovskimarko6@gmail.com>
2025-12-14 11:40:32 +0100

    8.cpp

--- Aud/Aud 8/8.cpp
+++ Aud/Aud 8/8.cpp
@@ -1,3 +1,13 @@
-//
-// Created by metro on 11/16/2025.
-//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[100];
+    int broj;
+    while (cin >> broj) {
+        a[broj]++;
+    }
+    for (int i = 0; i < 10; i++) {
+        cout<<i<<" "<<a[i]<<endl;
+    }
+}
