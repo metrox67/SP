da846e4257c3def437f086b1e8304b96ea617c43
Marko <zdravkovskimarko6@gmail.com>
2025-12-14 11:39:51 +0100

    7.cpp

--- Aud/Aud 8/7.cpp
+++ Aud/Aud 8/7.cpp
@@ -1,3 +1,25 @@
-//
-// Created by metro on 11/16/2025.
-//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, a[100];
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++) {
+        //a[i]
+        for (int j = i + 1; j < n; j++) {
+            if (a[i] == a[j]) {
+                for (int k = j; k < n-1; k++) {
+                    a[k] = a[k + 1];
+                }
+                n--;
+                j--;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        cout<< a[i]<<" ";
+    }
+}
