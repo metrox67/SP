4d46f1bbb5f094fa08640527aefc89d46b528882
Marko <zdravkovskimarko6@gmail.com>
2025-12-14 11:53:43 +0100

    7.cpp

--- Aud/Aud 10/7.cpp
+++ Aud/Aud 10/7.cpp
@@ -1,3 +1,23 @@
-//
-// Created by metro on 11/16/2025.
-//
+#include <iostream>
+using namespace std;
+
+
+int NZD(int a, int b) {
+    if (a%b==0) {
+        return b;
+    }
+    return NZD(b, a%b);
+}
+
+int main() {
+    int n, a[100];
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    int nzd = NZD(a[0], a[1]);
+    for (int i = 2; i < n; i++) {
+        nzd = NZD(nzd, a[i]);
+    }
+    cout << nzd;
+}
