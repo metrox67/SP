82d712d4d6eefcaa9bc0d074816b754196d86a37
Marko <zdravkovskimarko6@gmail.com>
2025-12-14 11:34:20 +0100

    3.cpp

--- Aud/Aud 8/3.cpp
+++ Aud/Aud 8/3.cpp
@@ -1,3 +1,19 @@
-//
-// Created by metro on 11/16/2025.
-//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[100], b[100];
+    int n;
+    cin>>n;
+    for (int i = 0; i < n; i++) {
+        cin>>a[i];
+    }
+    for (int i = 0; i < n; i++) {
+        cin>>b[i];
+    }
+    int skalaren_proizvod=0;
+    for (int i = 0; i < n; i++) {
+        skalaren_proizvod+=a[i]*b[i];
+    }
+    cout<<skalaren_proizvod<<endl;
+}
