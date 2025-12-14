14b44ce4b8497f1b49f3dd798cf4f56d760ae755
Marko <zdravkovskimarko6@gmail.com>
2025-12-14 11:50:53 +0100

    3.cpp

--- Aud/Aud 10/3.cpp
+++ Aud/Aud 10/3.cpp
@@ -1,3 +1,34 @@
-//
-// Created by metro on 11/16/2025.
-//
+#include <iostream>
+using namespace std;
+
+// Zadacava ne e kompletna
+
+bool dali_prost(int k, int m) {
+    if (k<4) {
+        return 1;
+    }
+    if (k%2 == 0) {
+        return 0;
+    }
+    if (k%m==0) {
+        return 0;
+    }
+    if (m>k/2) {
+        return 1;
+    }
+    return dali_prost(k, m+2);
+}
+
+int sleden_p_prost(int k) {
+    if (dali_prost(k)) {
+        return k;
+    }
+    return sleden_p_prost(sleden_p_prost(k+1));
+}
+
+int main() {
+    int n;
+    cin >> n;
+    sleden_p_prost(n);
+    cout<<sleden_p_prost(n+1)-n<<endl;
+}
