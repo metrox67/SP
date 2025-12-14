3e7765774ebb24c982f95a90ba5281b0afdc0551
Marko <zdravkovskimarko6@gmail.com>
2025-12-14 11:52:23 +0100

    6.cpp

--- Aud/Aud 10/6.cpp
+++ Aud/Aud 10/6.cpp
@@ -1,3 +1,21 @@
-//
-// Created by metro on 11/16/2025.
-//
+#include <iostream>
+using namespace std;
+
+int broj_8ci(int k) {
+    if (k == 0) {
+        return 0;
+    }
+    if (k % 10 == 8 && (k/10)%10 == 8) {
+        return 2 + broj_8ci(k/10);
+    }
+    if (k % 10 == 8) {
+        return 1 + broj_8ci(k / 10);
+    }
+    return 0 + broj_8ci(k / 10);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << broj_8ci(n);
+}
