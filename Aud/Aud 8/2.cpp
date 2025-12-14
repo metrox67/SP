5172a1bb14f408e30b9ae589b80304224492deb2
Marko <zdravkovskimarko6@gmail.com>
2025-12-14 11:33:35 +0100

    2.cpp

--- Aud/Aud 8/2.cpp
+++ Aud/Aud 8/2.cpp
@@ -1,3 +1,25 @@
-//
-// Created by metro on 11/16/2025.
-//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[100];
+    int br_a;
+    cin>>br_a;
+    for (int i=0; i<br_a; i++) {
+        cin>>a[i];
+    }
+    int suma_parni=0, suma_neparni=0, br_parni=0, br_neparni=0;
+    for (int i=0; i<br_a; i++) {
+        if (a[i]%2==0) {
+            suma_parni+=a[i];
+            br_parni++;
+        }
+        else {
+            suma_neparni+=a[i];
+            br_neparni++;
+        }
+    }
+    cout<<"suma parni: "<<suma_parni<<endl;
+    cout<<"Suma neparni: "<<suma_neparni<<endl;
+    cout<<"Odnos parni/neparni: "<<(float)br_parni/br_neparni<<endl;
+}
