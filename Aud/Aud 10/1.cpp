1ad93061893deb394e0e06df40090ced3b9bf931
Marko <zdravkovskimarko6@gmail.com>
2025-12-14 11:47:07 +0100

    1.cpp

--- Aud/Aud 10/1.cpp
+++ Aud/Aud 10/1.cpp
@@ -1,3 +1,28 @@
-//
-// Created by metro on 11/16/2025.
-//
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int suma_pr_br(int i) {
+    if (i == 1) {
+        return 1;
+    }
+    return i + suma_pr_br(i - 1);
+}
+
+int faktorijel(int i) {
+    if (i == 1) {
+        return 1;
+    }
+    return i * faktorijel(i - 1);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int rez = 0;
+    for (int i = 1; i < n; i++) {
+        int suma = suma_pr_br(i);
+        rez += faktorijel(suma);
+    }
+    cout << rez;
+}
