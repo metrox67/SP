2622e2a30c4d03506faca9f8fee2870547ed69ae
Marko <zdravkovskimarko6@gmail.com>
2025-12-17 14:35:42 +0100

    Update 1.cpp

--- Aud/Aud 11/1.cpp
+++ Aud/Aud 11/1.cpp
@@ -1,3 +1,15 @@
-//
-// Created by metro on 11/16/2025.
-//
+#include <iostream>
+using namespace std;
+
+void zamena(int *k, int *m) {
+    int tmp = *k;
+    *k = *m;
+    *m = tmp;
+}
+
+int main() {
+    int a = 5, b = 10;
+    cout<<"a"<<":"<<a<<" "<<"b"<<":"<<b<<endl;
+    zamena(&a, &b);
+    cout<<"a"<<":"<<a<<" "<<"b"<<":"<<b<<endl;
+}
